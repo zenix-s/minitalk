a940a075100e52bd1c77addc052d7e58d532260b
serferna <setfernet@gmail.com>
2024-06-05 18:04:09 +0200

    to the moon

--- src/minitalk.h
+++ src/minitalk.h
@@ -1,13 +1,16 @@
 #ifndef MINITALK_H
-# define MINITALK_H
+#define MINITALK_H
 
 // write, read, getpid, kill, pause
-# include <unistd.h>
+#include <unistd.h>
 
-// printf, scanf
-# include <stdio.h>
+// // printf, scanf
+// # include <stdio.h>
 
 // signal, sigaction, sigemptyset, sigaddset
 #include <signal.h>
+
+// ft_printf
+#include "./lib/printf/src/ft_printf.h"
 
 #endif
