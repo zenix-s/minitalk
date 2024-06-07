c4ffd1b429681c5eeb0e6d11464f5f48aee659ae
serferna <setfernet@gmail.com>
2024-06-07 18:48:01 +0200

    up
    u

--- src/app/server.c
+++ src/app/server.c
@@ -1,47 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 11:31:03 by serferna          #+#    #+#             */
+/*   Updated: 2024/06/07 11:31:05 by serferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minitalk.h"
+#include <signal.h>
 #include <unistd.h>
 
-t_bit_buffer g_bit_buffer = {0, 0};
+t_bit_buffer	g_bit_buffer = {0, 0};
 
-static void signal_handler(int sig, siginfo_t *info, void *context) {
-  (void)context;
-  (void)info;
-
-  if (sig == SIGUSR1) {
-    g_bit_buffer.byte |= (0 << g_bit_buffer.bit);
-  } else if (sig == SIGUSR2) {
-    g_bit_buffer.byte |= (1 << g_bit_buffer.bit);
-  }
-  g_bit_buffer.bit++;
-
-  if (g_bit_buffer.bit >= 8) {
-    if (g_bit_buffer.byte != 0) {
-      write(1, &g_bit_buffer.byte, 1);
-    }
-    g_bit_buffer.byte = 0;
-    g_bit_buffer.bit = 0;
-  }
+static void	signal_handler(int sig, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	if (sig == SIGUSR1)
+	{
+		g_bit_buffer.byte |= (0 << g_bit_buffer.bit);
+	}
+	else if (sig == SIGUSR2)
+	{
+		g_bit_buffer.byte |= (1 << g_bit_buffer.bit);
+	}
+	g_bit_buffer.bit++;
+	if (g_bit_buffer.bit >= 8)
+	{
+		if (g_bit_buffer.byte != 0)
+		{
+			write(1, &g_bit_buffer.byte, 1);
+		}
+		g_bit_buffer.byte = 0;
+		g_bit_buffer.bit = 0;
+	}
 }
 
-void init_sigaction(void) {
-  struct sigaction sa;
-  sa.sa_sigaction = &signal_handler;
-  sa.sa_flags = SA_SIGINFO;
-  if (sigaction(SIGUSR1, &sa, 0) == -1) {
-    ft_printf("Error\n");
-  }
-  if (sigaction(SIGUSR2, &sa, 0) == -1) {
-    ft_printf("Error\n");
-  }
+void	init_sigaction(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, 0);
+	sigaction(SIGUSR2, &sa, 0);
 }
 
-int main(void) {
-  pid_t pid = getpid();
-  ft_printf("Id del servidor: %d\n", pid);
-  while (1) {
-    init_sigaction();
-    sleep(1);
-  }
+int	main(void)
+{
+	pid_t	pid;
 
-  return 0;
+	pid = getpid();
+	ft_printf("Id del servidor: %d\n", pid);
+	while (1)
+	{
+		init_sigaction();
+	}
+	return (0);
 }
--- src/minitalk.h
+++ src/minitalk.h
@@ -1,23 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 11:31:11 by serferna          #+#    #+#             */
+/*   Updated: 2024/06/07 11:31:57 by serferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
-#define MINITALK_H
-
-typedef struct s_bit_buffer {
-  int byte;
-  int bit;
-} t_bit_buffer;
-
-typedef enum s_bool {
-  FALSE,
-  TRUE
-} t_bool;
+# define MINITALK_H
 
 // write, getpid, kill, pause
-#include <unistd.h>
+# include <unistd.h>
 
 // signal, sigaction, sigemptyset, sigaddset
-#include <signal.h>
+# include <signal.h>
 
 // ft_printf
-#include "./lib/printf/src/ft_printf.h"
+# include "./lib/printf/src/ft_printf.h"
+
+typedef struct s_bit_buffer
+{
+	int	byte;
+	int	bit;
+}		t_bit_buffer;
+
+typedef enum s_bool
+{
+	FALSE,
+	TRUE
+}		t_bool;
 
 #endif
