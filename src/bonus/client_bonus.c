51c95dc938a48b253b3d79e992d5cdb2a66deda8
zenix-s <sergioffdev@gmail.com>
2024-06-24 22:02:02 +0200

    bonus

--- src/bonus/client_bonus.c
+++ src/bonus/client_bonus.c
@@ -6,14 +6,121 @@
 /*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 01:13:32 by serferna          #+#    #+#             */
-/*   Updated: 2024/06/24 01:13:32 by serferna         ###   ########.fr       */
+/*   Updated: 2024/06/24 12:48:44 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minitalk.h"
 
-int    main(void)
+# include <signal.h>
+
+# include "../lib/libft/libft.h"
+
+void	error(char *msg)
 {
-    ft_printf("Hola mundo");
+	ft_putstr_fd(msg, 2);
+	exit(1);
+}
+
+t_bool	check_args(const int argc, const char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+		return (FALSE);
+	while (argv[1][i] != '\0')
+	{
+		if (argv[1][i] < '0' || argv[1][i] > '9')
+			return (FALSE);
+		i++;
+	}
+	if (ft_atoi(argv[1]) <= 0)
+		return (FALSE);
+	return (TRUE);
+}
+
+void	send_signal(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if (c & (1 << bit))
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error("Error occurred: failed to send signal");
+			else if (kill(pid, SIGUSR1) == -1)
+				error("Error occurred: failed to send signal");
+		}
+		// usleep(100);
+		pause();
+		bit++;
+	}
+}
+
+static void	signal_handler(const int sig, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	ft_printf("Hola: %d\n", sig);
+}
+
+int    main(const int argc, const char **argv)
+{
+	(void)argv;
+	struct sigaction	sa;
+	int	byte;
+	int server_id;
+
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("Id del servidor: %d\n", pid);
+
+	(void)byte;
+	sa.sa_sigaction = &signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, 0);
+	sigaction(SIGUSR2, &sa, 0);
+
+	if (!check_args(argc, argv))
+		error("Error occurred: invalid arguments");
+	server_id = ft_atoi(argv[1]);
+	// kill(server_id, SIGUSR1);
+	// pause();
+	// kill(server_id, SIGUSR1);
+	// pause();
+	// kill(server_id, SIGUSR1);
+	// pause();
+	// kill(server_id, SIGUSR1);
+	// pause();
+	// int i = 0;
+	// while(i < 8)
+	// {
+	// 	if (i % 2 == 0)
+	// 		kill(server_id, SIGUSR1);
+	// 	else
+	// 		kill(server_id, SIGUSR2);
+	// 	pause();
+	// 	i++;
+	// }
+	send_signal(server_id, 'c');
+
+
+
+	// byte = 0;
+	// while (argv[2][byte])
+	// {
+	// 	send_signal(ft_atoi(argv[1]), argv[2][byte]);
+	// 	byte++;
+	// }
+	// send_signal(ft_atoi(argv[1]), '\0');
+
+	// while(1)
+	// 	pause();
     return 0;
 }
--- src/bonus/server_bonus.c
+++ src/bonus/server_bonus.c
@@ -6,14 +6,44 @@
 /*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 01:13:40 by serferna          #+#    #+#             */
-/*   Updated: 2024/06/24 01:49:31 by serferna         ###   ########.fr       */
+/*   Updated: 2024/06/24 12:31:00 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minitalk.h"
+# include <signal.h>
+
+# include "../lib/libft/libft.h"
+
+
+static void	signal_handler(const int sig, siginfo_t *info, void *context)
+{
+    (void)context;
+    ft_printf("Hola: %d from %d\n", sig, (int)info->si_pid);
+	kill(info->si_pid, SIGUSR1);
+}
+
+void	init_sigaction(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, 0);
+	sigaction(SIGUSR2, &sa, 0);
+}
 
 int    main(void)
 {
-    ft_printf("Hola mundo");
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("Id del servidor: %d\n", pid);
+	init_sigaction();
+
+	while(1)
+		pause();
     return 0;
 }
